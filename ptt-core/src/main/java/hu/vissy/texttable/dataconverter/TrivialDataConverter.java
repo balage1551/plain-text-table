22aedae0a067c323b74af9cded557175b287d719
BALAGE-PC\Balage <bvissy@pillarmernokiroda.hu>
2017-07-03 15:27:38 +0200

    % TrivialDataConverter is now returns null for nulls, instead of empty string for consistency

--- ptt-core/src/main/java/hu/vissy/texttable/dataconverter/TrivialDataConverter.java
+++ ptt-core/src/main/java/hu/vissy/texttable/dataconverter/TrivialDataConverter.java
@@ -12,7 +12,7 @@ public class TrivialDataConverter<T> implements DataConverter<T> {
 
     @Override
     public String convert(T data) {
-        return data == null ? "" : data.toString();
+        return data == null ? null : data.toString();
     }
 
 }
